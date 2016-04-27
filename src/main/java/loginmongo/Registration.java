aaf8b9adfc2c05887b5e079af986465fe03d0876
Dineesha <dinsuraw@gmail.com>
2016-04-27 17:44:47 +0530

    Change connection to mongodb

--- src/main/java/loginmongo/Registration.java
+++ src/main/java/loginmongo/Registration.java
@@ -1,5 +1,9 @@
 package loginmongo;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
 import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServlet;
@@ -15,12 +19,12 @@ import java.sql.SQLException;
 /**
  * Created by hsenid on 4/25/16.
  */
-public class Registration extends HttpServlet{
+public class Registration extends HttpServlet {
     final static Logger logger = Logger.getLogger(Registration.class);
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
+       // PrintWriter out = response.getWriter();
 
         String fname = request.getParameter("fname");
         String lname = request.getParameter("lname");
@@ -31,7 +35,7 @@ public class Registration extends HttpServlet{
         String username = request.getParameter("uname");
         String password = request.getParameter("password");
 
-        try{
+        /*try{
 
             //loading drivers for mysql
             Class.forName("com.mysql.jdbc.Driver");
@@ -64,6 +68,35 @@ public class Registration extends HttpServlet{
             se.printStackTrace();
             logger.error("data not inserted");
         }
-    }
+    }*/
 
+        MongoClient mongo = new MongoClient("localhost", 27017);
+        DB db = mongo.getDB("login_form");
+        DBCollection coll = db.getCollection("test_user1");
+
+
+
+
+        BasicDBObject doc = new BasicDBObject();
+
+        doc.put("fname", fname);
+        doc.put("lname", lname);
+        doc.put("email", email);
+        doc.put("dob", dob);
+        doc.put("phone", phone);
+        doc.put("country", country);
+        doc.put("username", username);
+        doc.put("password", password);
+
+
+
+        //DBCollection coll = db.createCollection("test_user1", doc);
+
+
+
+
+        coll.insert(doc);
+
+
+    }
 }
